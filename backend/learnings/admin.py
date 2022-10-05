0dfe24438a9aa730b000383665a34f328d2f586c
presyos <preciousgift.canovas@sun-asterisk.com>
2022-10-05 14:59:50 +0800

    [SELS_TASK] Create the Models from ERD

--- backend/learnings/admin.py
+++ backend/learnings/admin.py
@@ -1,3 +1,9 @@
 from django.contrib import admin
-
-# Register your models here.
+from .models import EUser, Follow, Category, QuizTaken, Word, Choices, UserAnswer
+admin.site.register(EUser)
+admin.site.register(Follow)
+admin.site.register(Category)
+admin.site.register(QuizTaken)
+admin.site.register(Word)
+admin.site.register(Choices)
+admin.site.register(UserAnswer)
