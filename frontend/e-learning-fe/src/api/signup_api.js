fe7319e2df08479fa1d132b8f94b69d313c5d150
presyos <112840596+presyos@users.noreply.github.com>
2022-10-10 10:05:03 +0800

    [SELS_TASK] [FE/BE] SIGN UP - Connect to the api (#6)

---
+++ frontend/e-learning-fe/src/api/signup_api.js
@@ -0,0 +1,25 @@
+import axios from "axios";
+
+export default {
+  async signUp(query) {
+    const {
+      username,
+      firstname,
+      lastname,
+      email,
+      password,
+      confirmPassword: confirm_password,
+      isAdmin: is_admin,
+    } = query;
+    const response = await axios.post("http://127.0.0.1:8000/register/", {
+      username,
+      firstname,
+      lastname,
+      email,
+      password,
+      confirm_password,
+      is_admin,
+    });
+    return response;
+  },
+};
--- frontend/e-learning-fe/src/components/signup.js
+++ frontend/e-learning-fe/src/components/signup.js
@@ -1,17 +1,91 @@
-import React from "react";
+import React, { useState } from "react";
+
+import { ToastContainer, toast } from "react-toastify";
+import "react-toastify/dist/ReactToastify.css";
+
+import API from "../api";
 
 function SignUp() {
+  const [username, setUsername] = useState("");
+  const [email, setEmail] = useState("");
+  const [firstname, setFirstName] = useState("");
+  const [lastname, setLastName] = useState("");
+  const [password, setPassword] = useState("");
+  const [confirmPassword, setConfirmPassword] = useState("");
+  const [isAdmin, setIsAdmin] = useState(false);
+  const [isDisableSignupButton, setisDisableSignupButton] = useState(false);
+
+  const handleOnChangeAdmin = () => {
+    setIsAdmin(!isAdmin);
+  };
+
+  const handleSignUp = async () => {
+    if (password !== confirmPassword) {
+      toast.warn("Password Does Not Match!", {
+        position: "top-right",
+        autoClose: 5000,
+        hideProgressBar: false,
+        closeOnClick: true,
+        pauseOnHover: true,
+        draggable: true,
+        progress: undefined,
+      });
+    } else {
+      const response = await API.signup.signUp({
+        username,
+        email,
+        firstname,
+        lastname,
+        password,
+        confirmPassword,
+        isAdmin,
+      });
+      toast.success("Success!", {
+        position: "top-right",
+        autoClose: 5000,
+        hideProgressBar: false,
+        closeOnClick: true,
+        pauseOnHover: true,
+        draggable: true,
+        progress: undefined,
+      });
+    }
+  };
+
+  const checkFields = () => {
+    if (
+      username !== "" &&
+      email !== "" &&
+      firstname !== "" &&
+      lastname !== "" &&
+      password !== "" &&
+      confirmPassword !== ""
+    )
+      setisDisableSignupButton(true);
+  };
+
   return (
     <section>
+      <ToastContainer
+        position="top-right"
+        autoClose={5000}
+        hideProgressBar={false}
+        newestOnTop={false}
+        closeOnClick
+        rtl={false}
+        pauseOnFocusLoss
+        draggable
+        pauseOnHover
+      />
       <div class="flex flex-col items-center justify-center px-6 py-8 mx-auto md:h-screen">
         <div class="w-3/4 h-4/5  bg-white rounded-lg shadow dark:border dark:bg-gray-300 dark:border-gray-700">
           <div class="p-6 space-y-4 md:space-y-6 sm:p-8">
             <h1 class="text-xl font-bold leading-tight tracking-tight text-gray-900 md:text-2xl dark:text-black">
               Create E-Learning Account
             </h1>
-            <form class="space-y-4 md:space-y-6" action="#">
+            <div class="space-y-44 md:space-y-6">
               <div class="flex flex-row space-x-44">
-                <div>
+                <div class="w-96">
                   <label
                     for="username"
                     class="block mb-2 text-sm font-medium text-gray-900 dark:text-black"
@@ -22,14 +96,17 @@ function SignUp() {
                     type="text"
                     name="username"
                     id="username"
-                    class="bg-red-50 border border-red-200 text-gray-900 w-96
+                    class="bg-red-50 border border-red-200 text-gray-900
                       sm:text-sm rounded-lg focus:ring-primary-600 focus:border-primary-600 block w-full p-2.5 dark:bg-gray-200 
                       dark:border-gray-600 dark:placeholder-gray-400 dark:text-black dark:focus:ring-blue-500 dark:focus:border-blue-500"
                     placeholder="username"
-                    required
+                    onChange={(e) => {
+                      setUsername(e.target.value);
+                      checkFields();
+                    }}
                   />
                 </div>
-                <div>
+                <div class="w-96">
                   <label
                     for="email"
                     class="block mb-2 text-sm font-medium text-gray-900 dark:text-black"
@@ -40,17 +117,20 @@ function SignUp() {
                     type="email"
                     name="email"
                     id="email"
-                    class="bg-gray-50 border border-gray-300 text-gray-900 w-96
+                    class="bg-gray-50 border border-gray-300 text-gray-900
                       sm:text-sm rounded-lg focus:ring-primary-600 focus:border-primary-600 block w-full p-2.5 dark:bg-gray-200 
                       dark:border-gray-600 dark:placeholder-gray-400 dark:text-black dark:focus:ring-blue-500 dark:focus:border-blue-500"
                     placeholder="name@company.com"
-                    required
+                    onChange={(e) => {
+                      setEmail(e.target.value);
+                      checkFields();
+                    }}
                   />
                 </div>
               </div>
 
               <div class="flex flex-row space-x-44">
-                <div>
+                <div class="w-96">
                   <label
                     for="firstname"
                     class="block mb-2 text-sm font-medium text-gray-900 dark:text-black"
@@ -61,14 +141,17 @@ function SignUp() {
                     type="text"
                     name="firstname"
                     id="firstname"
-                    class="bg-gray-50 border border-gray-300 text-gray-900  w-96
+                    class="bg-gray-50 border border-gray-300 text-gray-900
                       sm:text-sm rounded-lg focus:ring-primary-600 focus:border-primary-600 block w-full p-2.5 dark:bg-gray-200 
                       dark:border-gray-600 dark:placeholder-gray-400 dark:text-black dark:focus:ring-blue-500 dark:focus:border-blue-500"
                     placeholder="firstname"
-                    required
+                    onChange={(e) => {
+                      setFirstName(e.target.value);
+                      checkFields();
+                    }}
                   />
                 </div>
-                <div>
+                <div class="w-96">
                   <label
                     for="password"
                     class="block mb-2 text-sm font-medium text-gray-900 dark:text-black"
@@ -80,17 +163,20 @@ function SignUp() {
                     name="password"
                     id="password"
                     placeholder="••••••••"
-                    class="bg-gray-50 border border-gray-300 text-gray-900 w-96
+                    class="bg-gray-50 border border-gray-300 text-gray-900
                       sm:text-sm rounded-lg focus:ring-primary-600 focus:border-primary-600 block 
                       w-full p-2.5 dark:bg-gray-200 dark:border-gray-600 dark:placeholder-gray-400 
                       dark:text-black dark:focus:ring-blue-500 dark:focus:border-blue-500"
-                    required
+                    onChange={(e) => {
+                      setPassword(e.target.value);
+                      checkFields();
+                    }}
                   />
                 </div>
               </div>
 
               <div class="flex flex-row space-x-44">
-                <div>
+                <div class="w-96">
                   <label
                     for="lastname"
                     class="block mb-2 text-sm font-medium text-gray-900 dark:text-black"
@@ -101,14 +187,17 @@ function SignUp() {
                     type="text"
                     name="lastname"
                     id="lastname"
-                    class="bg-gray-50 border border-gray-300 text-gray-900 w-96
+                    class="bg-gray-50 border border-gray-300 text-gray-900
                       sm:text-sm rounded-lg focus:ring-primary-600 focus:border-primary-600 block w-full p-2.5 dark:bg-gray-200 
                       dark:border-gray-600 dark:placeholder-gray-400 dark:text-black dark:focus:ring-blue-500 dark:focus:border-blue-500"
                     placeholder="lastname"
-                    required
+                    onChange={(e) => {
+                      setLastName(e.target.value);
+                      checkFields();
+                    }}
                   />
                 </div>
-                <div>
+                <div class="w-96">
                   <label
                     for="confirm-password"
                     class="block mb-2 text-sm font-medium text-gray-900 dark:text-black"
@@ -120,11 +209,14 @@ function SignUp() {
                     name="confirm-password"
                     id="confirm-password"
                     placeholder="••••••••"
-                    class="bg-gray-50 border border-gray-300 text-gray-900 w-96
+                    class="bg-gray-50 border border-gray-300 text-gray-900
                       sm:text-sm rounded-lg focus:ring-primary-600 focus:border-primary-600 block 
                       w-full p-2.5 dark:bg-gray-200 dark:border-gray-600 dark:placeholder-gray-400 
                       dark:text-black dark:focus:ring-blue-500 dark:focus:border-blue-500"
-                    required
+                    onChange={(e) => {
+                      setConfirmPassword(e.target.value);
+                      checkFields();
+                    }}
                   />
                 </div>
               </div>
@@ -137,7 +229,8 @@ function SignUp() {
                     class="w-4 h-4 border border-gray-300 rounded bg-gray-50 
                         focus:ring-3 focus:ring-primary-300 dark:bg-gray-700 dark:border-gray-600 
                         dark:focus:ring-primary-600 dark:ring-offset-gray-800"
-                    required
+                    checked={isAdmin}
+                    onChange={handleOnChangeAdmin}
                   />
                 </div>
                 <div class="ml-3 text-sm">
@@ -149,15 +242,29 @@ function SignUp() {
                   </label>
                 </div>
               </div>
-              <button
-                type="submit"
-                class="w-96 text-black bg-primary-600 hover:bg-primary-700 
+              {isDisableSignupButton === true ? (
+                <button
+                  type="submit"
+                  class="w-96 text-black bg-primary-600 hover:bg-primary-700 
                 focus:ring-4 focus:outline-none focus:ring-primary-300 font-medium ml-72
                 rounded-lg text-sm px-5 py-2.5 text-center dark:bg-primary-600 bg-green-600
                 dark:hover:bg-primary-700 dark:focus:ring-primary-800"
-              >
-                Create Account
-              </button>
+                  onClick={handleSignUp}
+                >
+                  Create Account
+                </button>
+              ) : (
+                <button
+                  type="submit"
+                  class="w-96 text-black bg-primary-600 hover:bg-primary-700 
+                focus:ring-4 focus:outline-none focus:ring-primary-300 font-medium ml-72
+                rounded-lg text-sm px-5 py-2.5 text-center dark:bg-primary-600 bg-gray-600
+                dark:hover:bg-primary-700 dark:focus:ring-primary-800"
+                  disabled
+                >
+                  Create Account
+                </button>
+              )}
               <p class="text-sm font-light text-black dark:text-black">
                 Already have an account?{" "}
                 <a
@@ -167,7 +274,7 @@ function SignUp() {
                   Login here
                 </a>
               </p>
-            </form>
+            </div>
           </div>
         </div>
       </div>
