406020e7d2bfc09cfdcdf37da5c886e282877017
presyos <preciousgift.canovas@sun-asterisk.com>
2022-10-12 13:39:38 +0800

    [SELS-12] [BE] WORD - Create endpoint getting all words that belong in that specific category

--- backend/elearning/urls.py
+++ backend/elearning/urls.py
@@ -1,11 +1,17 @@
 from django.contrib import admin
 from django.urls import path
 
-from learnings.views import DisplayCategoryWithUserFilter, LoginView, RegisterView
+from learnings.views import (
+    DisplayCategoryWithUserFilter,
+    GetWordsPerCategory,
+    LoginView,
+    RegisterView,
+)
 
 urlpatterns = [
     path("admin/", admin.site.urls),
     path("auth/", LoginView.as_view()),
     path("register/", RegisterView.as_view()),
     path("category/<str:user>/", DisplayCategoryWithUserFilter.as_view()),
+    path("get_words/<int:category_id>", GetWordsPerCategory.as_view()),
 ]
