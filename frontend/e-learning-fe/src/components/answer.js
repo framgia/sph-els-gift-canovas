11c356cde1a1fd753d412209ddb96689e2c8718e
presyos <112840596+presyos@users.noreply.github.com>
2022-10-17 11:05:14 +0800

    [SELS-14] [FE/BE] ANSWER PAGE - Map the result so allow user to answer (#14)
    * [SELS-14] [FE/BE] ANSWER PAGE - Map the result so allow user to answer
    
    * remove useEffect for userAnswers

--- frontend/e-learning-fe/src/components/answer.js
+++ frontend/e-learning-fe/src/components/answer.js
@@ -1,13 +1,16 @@
 import React, { useEffect, useState } from "react";
-import Choices from "./choices";
 import API from "../api";
 
 function Answer() {
+  const [userAnswers, setUserAnswers] = useState([]);
   const [words, setWords] = useState([]);
-  const [category, setCategory] = useState([]);
+  const [loading, setLoading] = useState(false);
+  const [index, setIndex] = useState(0);
+  const [wordsLength, setwordsLength] = useState(0);
 
   const token = localStorage.getItem("token");
-  const category_id = 1;
+  const category_id = 1; //props for the category id
+  let newAnswer = [];
 
   const fetchData = async () => {
     await API.word
@@ -16,9 +19,24 @@ function Answer() {
         token,
       })
       .then((data) => {
-        setCategory(data.data.category_name);
-        setWords(data.data.words[0]);
+        setWords(data.data);
+        setwordsLength(data.data.words.length);
+        setLoading(true);
       });
+  };
+
+  const nextIndex = (answer) => {
+    if (index <= wordsLength - 1) {
+      newAnswer = {
+        category_id: words.words[index].category_id,
+        userAnswer: answer,
+        correct_answer: words.words[index].correct_answer,
+        word: words.words[index].word,
+        word_id: words.words[index].word_id,
+      };
+      setUserAnswers([...userAnswers, newAnswer]);
+      setIndex(index + 1);
+    }
   };
 
   useEffect(() => {
@@ -27,20 +45,68 @@ function Answer() {
 
   return (
     <div class="p-6">
-      <h5 class="mb-2 text-2xl font-bold tracking-tight text-gray-900 dark:text-black">
-        {category}
-      </h5>
-      <div class="flex flex-row place-content-left p-6 flex space-x-24 ">
-        <p class="text-4xl font-semibold text-gray-900 dark:text-blackl">
-          {words.word}
-        </p>
-        <div class="flex flex-col">
-          <Choices choice={words.choice_a} />
-          <Choices choice={words.choice_b} />
-          <Choices choice={words.choice_c} />
-          <Choices choice={words.choice_d} />
+      {loading ? (
+        <div>
+          {userAnswers.length !== wordsLength && (
+            <div>
+              <div class="flex flex-row space-x-72 ">
+                <h5 class="mb-2 text-2xl font-bold tracking-tight text-gray-900 dark:text-black">
+                  {words.category_name}
+                </h5>
+                <h4 class="mb-2 text-2xl font-bold tracking-tight text-gray-900 dark:text-black">
+                  {index + 1} of {wordsLength}
+                </h4>
+              </div>
+
+              <div class="flex flex-row place-content-left p-6 flex space-x-24 ">
+                <p class="text-4xl font-semibold text-gray-900 dark:text-blackl">
+                  {words.words[index].word}
+                </p>
+                <div class="flex flex-col">
+                  <button
+                    class="text-white bg-gradient-to-r from-blue-500 
+                via-blue-600 to-blue-700 hover:bg-gradient-to-br focus:ring-4 
+                focus:outline-none focus:ring-blue-300 dark:focus:ring-blue-800 
+                font-medium rounded-lg text-sm px-5 py-2.5 text-center mr-2 mb-2"
+                    onClick={() => nextIndex(words.words[index].choice_a)}
+                  >
+                    {words.words[index].choice_a}
+                  </button>
+                  <button
+                    class="text-white bg-gradient-to-r from-blue-500 
+                via-blue-600 to-blue-700 hover:bg-gradient-to-br focus:ring-4 
+                focus:outline-none focus:ring-blue-300 dark:focus:ring-blue-800 
+                font-medium rounded-lg text-sm px-5 py-2.5 text-center mr-2 mb-2"
+                    onClick={() => nextIndex(words.words[index].choice_b)}
+                  >
+                    {words.words[index].choice_b}
+                  </button>
+                  <button
+                    class="text-white bg-gradient-to-r from-blue-500 
+                via-blue-600 to-blue-700 hover:bg-gradient-to-br focus:ring-4 
+                focus:outline-none focus:ring-blue-300 dark:focus:ring-blue-800 
+                font-medium rounded-lg text-sm px-5 py-2.5 text-center mr-2 mb-2"
+                    onClick={() => nextIndex(words.words[index].choice_c)}
+                  >
+                    {words.words[index].choice_c}
+                  </button>
+                  <button
+                    class="text-white bg-gradient-to-r from-blue-500 
+                via-blue-600 to-blue-700 hover:bg-gradient-to-br focus:ring-4 
+                focus:outline-none focus:ring-blue-300 dark:focus:ring-blue-800 
+                font-medium rounded-lg text-sm px-5 py-2.5 text-center mr-2 mb-2"
+                    onClick={() => nextIndex(words.words[index].choice_d)}
+                  >
+                    {words.words[index].choice_d}
+                  </button>
+                </div>
+              </div>
+            </div>
+          )}
         </div>
-      </div>
+      ) : (
+        ""
+      )}
     </div>
   );
 }
