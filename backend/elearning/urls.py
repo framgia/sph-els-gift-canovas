e49cd396cebc7723e4c5cd54c6dc7124e84ee1ac
presyos <112840596+presyos@users.noreply.github.com>
2022-10-21 15:44:13 +0800

    [SELS-29] [BE] FOLLOWER - Create endpoint to add a new follower (#29)

--- backend/elearning/urls.py
+++ backend/elearning/urls.py
@@ -2,6 +2,7 @@ from django.contrib import admin
 from django.urls import path
 
 from learnings.views import (
+    AddNewFollower,
     DisplayCategoryWithUserFilter,
     EditUserDetails,
     GetResults,
@@ -19,12 +20,13 @@ from learnings.views import (
 urlpatterns = [
     path("admin/", admin.site.urls),
     path("auth/", LoginView.as_view()),
-    path("register/", RegisterView.as_view()),
     path("category/<str:user>/", DisplayCategoryWithUserFilter.as_view()),
+    path("follow/", AddNewFollower.as_view()),
     path("get_words/<int:category_id>", GetWordsPerCategory.as_view()),
-    path("user_answer/", UserAnswerView.as_view()),
-    path("results/<int:quiz_taken_id>", GetResults.as_view()),
     path("not_admin_users/", NotAdminUserList.as_view()),
+    path("register/", RegisterView.as_view()),
+    path("results/<int:quiz_taken_id>", GetResults.as_view()),
+    path("user_answer/", UserAnswerView.as_view()),
     path("user_details/<str:username>/", UserDetails.as_view()),
     path("edit_user_details/<int:id>/", EditUserDetails.as_view()),
     path("number_of_categories_taken/<str:username>/", NumberOfCategoriesTaken.as_view()),
