2ffbd8f7e69613acbdd9d8f9fcce3e44205b4ad4
presyos <preciousgift.canovas@sun-asterisk.com>
2022-10-24 14:42:48 +0800

    [SELS-39] [FE/BE] WORDS LEARNED PAGE - Create component for words learned page

--- frontend/e-learning-fe/src/api/word.js
+++ frontend/e-learning-fe/src/api/word.js
@@ -10,4 +10,16 @@ export default {
     });
     return response;
   },
+  async getWordsLearned(query) {
+    const { username, token } = query;
+    const response = await axios.get(
+      `http://127.0.0.1:8000/words_learned/${username}`,
+      {
+        headers: {
+          Authorization: `Token ${token}`,
+        },
+      }
+    );
+    return response;
+  },
 };
