d434171c735bd3883ab1e6253aea831449cf2f5f
presyos <preciousgift.canovas@sun-asterisk.com>
2022-11-04 19:54:31 +0800

    added loader component and fixed the login component

--- frontend/e-learning-fe/src/components/login.js
+++ frontend/e-learning-fe/src/components/login.js
@@ -4,16 +4,20 @@ import { ToastContainer, toast } from "react-toastify";
 import "react-toastify/dist/ReactToastify.css";
 
 import API from "../api";
+import Loader from "./loader";
 
 function Login() {
   const navigate = useNavigate();
   const [username, setUsername] = useState("");
   const [password, setPassword] = useState("");
-  const [isDisable, setIsDisable] = useState(false);
+  const [isDisable, setIsDisable] = useState(true);
   const [isLoading, setIsLoading] = useState(false);
+  const [isDisableField, setIsDisableField] = useState(false);
 
   const handleLogin = async () => {
     setIsLoading(true);
+    setIsDisableField(true);
+    setIsDisable(true);
     const response = await API.login.login({
       username,
       password,
@@ -31,6 +35,8 @@ function Login() {
       } else navigate("/dashboard", { replace: true });
     } else {
       setIsLoading(false);
+      setIsDisableField(false);
+      setIsDisable(false);
       toast.error("Check Credentials!", {
         position: "top-right",
         autoClose: 5000,
@@ -44,7 +50,7 @@ function Login() {
   };
 
   const checkFields = () => {
-    if (username !== "" && password !== "") setIsDisable(true);
+    if (username !== "" && password !== "") setIsDisable(false);
   };
 
   useEffect(() => {
@@ -70,27 +76,7 @@ function Login() {
         pauseOnHover
       />
       <div class="flex flex-col items-center justify-center px-6 py-8 mx-auto md:h-screen lg:py-0">
-        {isLoading ? (
-          <div>
-            <svg
-              class="inline mr-2 w-10 h-10 text-gray-200 animate-spin dark:text-gray-600 fill-blue-600"
-              viewBox="0 0 100 101"
-              fill="none"
-              xmlns="http://www.w3.org/2000/svg"
-            >
-              <path
-                d="M100 50.5908C100 78.2051 77.6142 100.591 50 100.591C22.3858 100.591 0 78.2051 0 50.5908C0 22.9766 22.3858 0.59082 50 0.59082C77.6142 0.59082 100 22.9766 100 50.5908ZM9.08144 50.5908C9.08144 73.1895 27.4013 91.5094 50 91.5094C72.5987 91.5094 90.9186 73.1895 90.9186 50.5908C90.9186 27.9921 72.5987 9.67226 50 9.67226C27.4013 9.67226 9.08144 27.9921 9.08144 50.5908Z"
-                fill="currentColor"
-              />
-              <path
-                d="M93.9676 39.0409C96.393 38.4038 97.8624 35.9116 97.0079 33.5539C95.2932 28.8227 92.871 24.3692 89.8167 20.348C85.8452 15.1192 80.8826 10.7238 75.2124 7.41289C69.5422 4.10194 63.2754 1.94025 56.7698 1.05124C51.7666 0.367541 46.6976 0.446843 41.7345 1.27873C39.2613 1.69328 37.813 4.19778 38.4501 6.62326C39.0873 9.04874 41.5694 10.4717 44.0505 10.1071C47.8511 9.54855 51.7191 9.52689 55.5402 10.0491C60.8642 10.7766 65.9928 12.5457 70.6331 15.2552C75.2735 17.9648 79.3347 21.5619 82.5849 25.841C84.9175 28.9121 86.7997 32.2913 88.1811 35.8758C89.083 38.2158 91.5421 39.6781 93.9676 39.0409Z"
-                fill="currentFill"
-              />
-            </svg>
-          </div>
-        ) : (
-          ""
-        )}
+        {isLoading ? <Loader /> : ""}
         <p class="flex items-center mb-6 text-2xl font-semibold text-gray-900 dark:text-white mt-5">
           E-Learning
         </p>
@@ -120,6 +106,7 @@ function Login() {
                   setUsername(e.target.value);
                   checkFields();
                 }}
+                disabled={isDisableField}
               />
             </div>
             <div>
@@ -142,20 +129,11 @@ function Login() {
                   setPassword(e.target.value);
                   checkFields();
                 }}
+                disabled={isDisableField}
               />
             </div>
 
             {isDisable === true ? (
-              <button
-                type="submit"
-                class="w-full text-white bg-primary-600 hover:bg-primary-700 focus:ring-4 
-                    focus:outline-none focus:ring-primary-300 font-medium rounded-lg text-sm px-5 py-2.5 text-center 
-                    dark:bg-blue-600 dark:hover:bg-primary-700 dark:focus:ring-primary-800"
-                onClick={handleLogin}
-              >
-                Sign in
-              </button>
-            ) : (
               <button
                 type="submit"
                 class="w-full text-white bg-primary-600 hover:bg-primary-700 focus:ring-4 
@@ -165,14 +143,23 @@ function Login() {
               >
                 Sign in
               </button>
+            ) : (
+              <button
+                type="submit"
+                class="w-full text-white bg-primary-600 hover:bg-primary-700 focus:ring-4 
+                  focus:outline-none focus:ring-primary-300 font-medium rounded-lg text-sm px-5 py-2.5 text-center 
+                  dark:bg-blue-600 dark:hover:bg-primary-700 dark:focus:ring-primary-800"
+                onClick={handleLogin}
+              >
+                Sign in
+              </button>
             )}
-
             <p class="text-sm font-light text-gray-500 dark:text-gray-400">
-              Don’t have an account yet?{" "}
+              Don’t have an account yet?
               <Link to="/signup">
-                <a class="font-medium text-primary-600 hover:underline dark:text-primary-500">
+                <p class="font-medium text-primary-600 hover:underline dark:text-primary-500">
                   Sign up
-                </a>{" "}
+                </p>
               </Link>
             </p>
           </div>
