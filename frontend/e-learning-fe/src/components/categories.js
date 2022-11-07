7cf8cc906f5b7e1885aa376d962d6e8f68795cbd
presyos <preciousgift.canovas@sun-asterisk.com>
2022-11-07 16:53:37 +0800

    [SELS-84] Add text if there are no categories, users, and words learned

--- frontend/e-learning-fe/src/components/categories.js
+++ frontend/e-learning-fe/src/components/categories.js
@@ -41,29 +41,33 @@ function Categories() {
         </div>
       ) : (
         <div class="flex flex-row w-full flex flex-wrap place-content-center">
-          {category.map((cat, i) => (
-            <div
-              key={i}
-              class="p-6 m-8 w-80 bg-white rounded-lg border border-gray-200 shadow-md dark:bg-gray-400 dark:border-gray-700"
-            >
-              <h5 class="mb-2 text-2xl font-bold tracking-tight text-black dark:text-black">
-                {cat.category_name}
-              </h5>
-
-              <p class="mb-3 font-normal text-white dark:text-white">
-                {cat.description}
-              </p>
-              <Link to="/answer" state={{ categoryId: cat.id }}>
-                <a
-                  class="text-center ml-56 w-24 py-2 px-3 text-sm font-medium  text-white bg-blue-700 
-              rounded-lg hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 dark:bg-blue-600
-               dark:hover:bg-blue-700 dark:focus:ring-blue-800"
+          {category.length === 0 ? (
+            <p class="text-4xl text-center mt-44 font-semibold">
+              No Categories
+            </p>
+          ) : (
+            <>
+              {category.map((cat, i) => (
+                <div
+                  key={i}
+                  class="p-6 m-8 w-80 bg-white rounded-lg border border-gray-200 shadow-md dark:bg-gray-400 dark:border-gray-700"
                 >
-                  Start
-                </a>
-              </Link>
-            </div>
-          ))}
+                  <h5 class="mb-2 text-2xl font-bold tracking-tight text-black dark:text-black">
+                    {cat.category_name}
+                  </h5>
+
+                  <p class="mb-3 font-normal text-white dark:text-white">
+                    {cat.description}
+                  </p>
+                  <Link to="/answer" state={{ categoryId: cat.id }}>
+                    <a class="text-center ml-56 w-24 py-2 px-3 text-sm font-medium  text-white bg-blue-700 rounded-lg hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800">
+                      Start
+                    </a>
+                  </Link>
+                </div>
+              ))}
+            </>
+          )}
         </div>
       )}
     </div>
--- frontend/e-learning-fe/src/components/notAdminUsers.js
+++ frontend/e-learning-fe/src/components/notAdminUsers.js
@@ -42,60 +42,60 @@ function NotAdminUsers() {
       {isLoading ? (
         ""
       ) : (
-        <div class="flex flex-col place-items-center">
-          <ul
-            class="w-8/12 text-xl font-medium text-gray-900 bg-white rounded-lg border
-           border-gray-200 dark:bg-gray-700 dark:border-gray-600 dark:text-white"
-          >
-            <div class="flex flex-row">
-              <li class="py-2 px-4 w-full rounded-t-lg border-b border-gray-200 dark:border-gray-600">
-                Username
-              </li>
-              <li class="py-2 px-4 w-full border-b border-gray-200 dark:border-gray-600">
-                First Name
-              </li>
-              <li class="py-2 px-4 w-full border-b border-gray-200 dark:border-gray-600">
-                Last Name
-              </li>
-              <li class="py-2 px-4 w-full border-b border-gray-200 dark:border-gray-600">
-                Email
-              </li>
+        <>
+          {users.length === 0 ? (
+            <p class="text-4xl text-center mt-44 font-semibold">No Users</p>
+          ) : (
+            <div class="flex flex-col place-items-center">
+              <ul class="w-8/12 text-xl font-medium text-gray-900 bg-white rounded-lg border border-gray-200 dark:bg-gray-700 dark:border-gray-600 dark:text-white">
+                <div class="flex flex-row">
+                  <li class="py-2 px-4 w-full rounded-t-lg border-b border-gray-200 dark:border-gray-600">
+                    Username
+                  </li>
+                  <li class="py-2 px-4 w-full border-b border-gray-200 dark:border-gray-600">
+                    First Name
+                  </li>
+                  <li class="py-2 px-4 w-full border-b border-gray-200 dark:border-gray-600">
+                    Last Name
+                  </li>
+                  <li class="py-2 px-4 w-full border-b border-gray-200 dark:border-gray-600">
+                    Email
+                  </li>
+                </div>
+              </ul>
+              <ul class="w-8/12 text-sm font-medium text-gray-900 bg-white rounded-lg border border-gray-200 dark:bg-gray-600 dark:border-gray-600 dark:text-white mt-8">
+                {users.map((data) => (
+                  <div class="flex flex-row">
+                    <li
+                      onClick={() => handleClickUser(data.username)}
+                      class="py-2 px-4 w-full rounded-t-lg border-b border-gray-200 dark:border-gray-600 cursor-pointer"
+                    >
+                      {data.username}
+                    </li>
+                    <li
+                      onClick={() => handleClickUser(data.username)}
+                      class="py-2 px-4 w-full rounded-t-lg border-b border-gray-200 dark:border-gray-600 cursor-pointer"
+                    >
+                      {data.firstname}
+                    </li>
+                    <li
+                      onClick={() => handleClickUser(data.username)}
+                      class="py-2 px-4 w-full rounded-t-lg border-b border-gray-200 dark:border-gray-600 cursor-pointer"
+                    >
+                      {data.lastname}
+                    </li>
+                    <li
+                      onClick={() => handleClickUser(data.username)}
+                      class="py-2 px-4 w-full rounded-t-lg border-b border-gray-200 dark:border-gray-600 cursor-pointer"
+                    >
+                      {data.email}
+                    </li>
+                  </div>
+                ))}
+              </ul>
             </div>
-          </ul>
-          <ul
-            class="w-8/12 text-sm font-medium text-gray-900 bg-white rounded-lg border
-           border-gray-200 dark:bg-gray-600 dark:border-gray-600 dark:text-white mt-8"
-          >
-            {users.map((data) => (
-              <div class="flex flex-row">
-                <li
-                  onClick={() => handleClickUser(data.username)}
-                  class="py-2 px-4 w-full rounded-t-lg border-b border-gray-200 dark:border-gray-600 cursor-pointer"
-                >
-                  {data.username}
-                </li>
-                <li
-                  onClick={() => handleClickUser(data.username)}
-                  class="py-2 px-4 w-full rounded-t-lg border-b border-gray-200 dark:border-gray-600 cursor-pointer"
-                >
-                  {data.firstname}
-                </li>
-                <li
-                  onClick={() => handleClickUser(data.username)}
-                  class="py-2 px-4 w-full rounded-t-lg border-b border-gray-200 dark:border-gray-600 cursor-pointer"
-                >
-                  {data.lastname}
-                </li>
-                <li
-                  onClick={() => handleClickUser(data.username)}
-                  class="py-2 px-4 w-full rounded-t-lg border-b border-gray-200 dark:border-gray-600 cursor-pointer"
-                >
-                  {data.email}
-                </li>
-              </div>
-            ))}
-          </ul>
-        </div>
+          )}
+        </>
       )}
     </div>
   );
