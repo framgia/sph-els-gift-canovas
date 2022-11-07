bca8c6459f2aed15aa46333192bb4a2bcc9159a5
presyos <preciousgift.canovas@sun-asterisk.com>
2022-11-07 11:03:43 +0800

    fixed sign in button

--- frontend/e-learning-fe/src/components/login.js
+++ frontend/e-learning-fe/src/components/login.js
@@ -129,14 +129,11 @@ function Login() {
                 disabled={isLoading}
               />
             </div>
-
             <button
               type="submit"
-              class={
-                isDisableSignInButton
-                  ? "w-full text-white bg-primary-600 hover:bg-primary-700 focus:ring-4 focus:outline-none focus:ring-primary-300 font-medium rounded-lg text-sm px-5 py-2.5 text-center dark:bg-gray-600 dark:hover:bg-primary-700 dark:focus:ring-primary-800"
-                  : "w-full text-white bg-primary-600 hover:bg-primary-700 focus:ring-4 focus:outline-none focus:ring-primary-300 font-medium rounded-lg text-sm px-5 py-2.5 text-center dark:bg-blue-600 dark:hover:bg-primary-700 dark:focus:ring-primary-800"
-              }
+              class={`w-full text-white bg-primary-600 hover:bg-primary-700 focus:ring-4 focus:outline-none focus:ring-primary-300 font-medium rounded-lg text-sm px-5 py-2.5 text-center dark:hover:bg-primary-700 dark:focus:ring-primary-800 ${
+                isDisableSignInButton ? "dark:bg-gray-600" : "dark:bg-blue-600"
+              }`}
               onClick={handleLogin}
               disabled={isDisableSignInButton}
             >
