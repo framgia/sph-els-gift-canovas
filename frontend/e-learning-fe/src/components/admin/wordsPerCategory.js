fd5fa57218bc7c6686b2d7af59884fc1c394e102
presyos <preciousgift.canovas@sun-asterisk.com>
2022-11-07 14:51:30 +0800

    [SELS-80] [FE] ADMIN - View words per category table alignment and fix cursor

--- frontend/e-learning-fe/src/components/admin/wordsPerCategory.js
+++ frontend/e-learning-fe/src/components/admin/wordsPerCategory.js
@@ -42,7 +42,7 @@ function AdminWordsPerCategory() {
   return (
     <div class="p-6 bg-gray-300 h-screen">
       <Navbar />
-      <h5 class="mb-2 mt-12 text-2xl font-bold tracking-tight text-gray-900 dark:text-black">
+      <h5 class="mb-2 mt-12 text-2xl font-bold tracking-tight text-gray-900 dark:text-black cursor-default">
         Words in Category : {words.category_name}
       </h5>
       {isLoading ? (
@@ -50,61 +50,60 @@ function AdminWordsPerCategory() {
       ) : (
         <div class="flex flex-col place-items-center mt-5">
           <ul
-            class="w-8/12 text-xl font-medium text-gray-900 bg-white rounded-lg border
+            class="w-full text-xl font-medium text-gray-900 bg-white rounded-lg border
          border-gray-200 dark:bg-gray-700 dark:border-gray-600 dark:text-white"
           >
-            <div class="flex flex-row">
-              <li class="py-2 px-4 w-full rounded-t-lg border-b border-gray-200 dark:border-gray-600">
+            <div class="flex flex-row  cursor-default">
+              <li class="py-2 px-4 w-6/12 rounded-t-lg border-b border-gray-200 dark:border-gray-600">
                 Word
               </li>
-              <li class="py-2 px-4 w-full border-b border-gray-200 dark:border-gray-600">
+              <li class="py-2 px-4 w-3/4 border-b border-gray-200 dark:border-gray-600">
                 Correct Answer
               </li>
-              <li class="py-2 px-4 w-full border-b border-gray-200 dark:border-gray-600">
+              <li class="py-2 px-4 w-3/4 border-b border-gray-200 dark:border-gray-600">
                 Choice A
               </li>
-              <li class="py-2 px-4 w-full border-b border-gray-200 dark:border-gray-600">
+              <li class="py-2 px-4 w-3/4 border-b border-gray-200 dark:border-gray-600">
                 Choice B
               </li>
-              <li class="py-2 px-4 w-full border-b border-gray-200 dark:border-gray-600">
+              <li class="py-2 px-4 w-3/4 border-b border-gray-200 dark:border-gray-600">
                 Choice C
               </li>
-              <li class="py-2 px-4 w-full border-b border-gray-200 dark:border-gray-600">
+              <li class="py-2 px-4 w-3/4 border-b border-gray-200 dark:border-gray-600">
                 Choice D
               </li>
-              <li class="py-2 px-4 w-full border-b border-gray-200 dark:border-gray-600">
+              <li class="py-2 px-4 w-3/4 border-b border-gray-200 dark:border-gray-600">
                 Action
               </li>
             </div>
           </ul>
           <ul
-            class="w-8/12 text-sm font-medium text-gray-900 bg-white rounded-lg border
+            class="w-full text-sm font-medium text-gray-900 bg-white rounded-lg border
          border-gray-200 dark:bg-gray-600 dark:border-gray-600 dark:text-white mt-8"
           >
             {words.words.map((word) => (
-              <div class="flex flex-row">
-                <li class="py-2 px-4 w-64 rounded-t-lg border-b border-gray-200 dark:border-gray-600 cursor-pointer">
+              <div class="flex flex-row cursor-default">
+                <li class="py-2 px-4 w-6/12 rounded-t-lg border-b border-gray-200 dark:border-gray-600">
                   {word.word}
                 </li>
-
-                <li class="py-2 px-4 w-64 text-center rounded-t-lg border-b border-gray-200 dark:border-gray-600">
+                <li class="py-2 px-4 w-3/4 border-b border-gray-200 dark:border-gray-600">
                   {word.correct_answer}
                 </li>
-                <li class="py-2 px-4 w-64 text-center rounded-t-lg border-b border-gray-200 dark:border-gray-600">
+                <li class="py-2 px-4 w-3/4 border-b border-gray-200 dark:border-gray-600">
                   {word.choice_a}
                 </li>
-                <li class="py-2 px-4 w-64 text-center rounded-t-lg border-b border-gray-200 dark:border-gray-600">
+                <li class="py-2 px-4 w-3/4 border-b border-gray-200 dark:border-gray-600">
                   {word.choice_b}
                 </li>
-                <li class="py-2 px-4 w-64 text-center rounded-t-lg border-b border-gray-200 dark:border-gray-600">
+                <li class="py-2 px-4 w-3/4 border-b border-gray-200 dark:border-gray-600">
                   {word.choice_c}
                 </li>
-                <li class="py-2 px-4 w-64 text-center rounded-t-lg border-b border-gray-200 dark:border-gray-600">
+                <li class="py-2 px-4 w-3/4 border-b border-gray-200 dark:border-gray-600">
                   {word.choice_d}
                 </li>
 
-                <li class="ml-20 py-2 px-4 w-64 rounded-t-lg border-b border-gray-200 dark:border-gray-600 cursor-pointer">
-                  <div class="flex flex-row">
+                <li class="py-2 px-4 w-3/4 border-b border-gray-200 dark:border-gray-600">
+                  <div class="flex flex-row  cursor-pointer">
                     <Link
                       to="/admin-edit-word-and-choices"
                       state={{ data: word }}
