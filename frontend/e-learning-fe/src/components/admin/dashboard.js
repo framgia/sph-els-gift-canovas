3ee4e1b1849dfb17627a95171c97081cefa05fcb
presyos <preciousgift.canovas@sun-asterisk.com>
2022-11-08 14:00:45 +0800

    place add button category inside loading condition

--- frontend/e-learning-fe/src/components/admin/dashboard.js
+++ frontend/e-learning-fe/src/components/admin/dashboard.js
@@ -40,25 +40,25 @@ function AdminDashboard() {
   return (
     <div class="p-6 bg-gray-300 h-screen">
       <Navbar />
-      <div class="flex flex-row justify-around">
-        <h5 class="mb-2 mt-12 text-2xl font-bold tracking-tight text-gray-900 dark:text-black cursor-default">
-          Categories
-        </h5>
-        <Link to="/admin-add-category">
-          <button class="mt-12 relative inline-flex items-center justify-center p-0.5 mb-2 mr-2 overflow-hidden text-sm font-medium text-gray-900 rounded-lg group bg-gradient-to-br from-cyan-500 to-blue-500 group-hover:from-cyan-500 group-hover:to-blue-500 hover:text-white dark:text-white focus:ring-4 focus:outline-none focus:ring-cyan-200 dark:focus:ring-cyan-800">
-            <span class="relative px-5 py-2.5 transition-all ease-in duration-75 bg-white dark:bg-gray-900 rounded-md group-hover:bg-opacity-0">
-              Add Category
-            </span>
-          </button>
-        </Link>
-      </div>
+      <h5 class="mt-12 text-2xl font-bold tracking-tight text-gray-900 dark:text-black">
+        Categories
+      </h5>
 
       {isLoading ? (
         <div class="text-center mt-44">
           <Loader />
         </div>
       ) : (
-        <div class="flex flex-col place-items-center mt-2">
+        <div class="flex flex-col place-items-center">
+          <div class="w-full flex justify-end">
+            <Link to="/admin-add-category">
+              <button class="mt-2 inline-flex items-center justify-center p-0.5 mb-2 mr-2 overflow-hidden text-sm font-medium text-gray-900 rounded-lg group bg-gradient-to-br from-cyan-500 to-blue-500 group-hover:from-cyan-500 group-hover:to-blue-500 hover:text-white dark:text-white focus:ring-4 focus:outline-none focus:ring-cyan-200 dark:focus:ring-cyan-800">
+                <span class="px-5 py-2.5 transition-all ease-in duration-75 bg-white dark:bg-gray-900 rounded-md group-hover:bg-opacity-0">
+                  Add Category
+                </span>
+              </button>
+            </Link>
+          </div>
           <ul
             class="w-8/12 text-xl font-medium text-gray-900 bg-white rounded-lg border
            border-gray-200 dark:bg-gray-700 dark:border-gray-600 dark:text-white"
