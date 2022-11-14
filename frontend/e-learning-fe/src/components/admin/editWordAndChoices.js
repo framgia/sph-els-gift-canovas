1c92e53a9d0b5a6499f066070351223f47d1ff8e
presyos <preciousgift.canovas@sun-asterisk.com>
2022-11-14 17:54:36 +0800

    [SELS-93] [FE] ADMIN - Edit Words and Choices

--- frontend/e-learning-fe/src/components/admin/editWordAndChoices.js
+++ frontend/e-learning-fe/src/components/admin/editWordAndChoices.js
@@ -51,7 +51,7 @@ function AdminEditWordAndChoices() {
   };
 
   return (
-    <div class="p-6 bg-gray-300 h-screen">
+    <div class="bg-gray-300 h-screen cursor-default">
       <ToastContainer
         position="top-right"
         autoClose={5000}
@@ -64,135 +64,136 @@ function AdminEditWordAndChoices() {
         pauseOnHover
       />
       <Navbar />
-      <p class="text-4xl font-bold text-gray-900 dark:text-black py-6 mt-12">
-        Edit Word
-      </p>
-      <div class="flex flex-row space-x-4 ">
-        <div class="flex flex-col">
-          <div>
-            <label
-              for="word"
-              class="block mb-2 text-sm font-medium text-black dark:text-black"
-            >
-              Word
-            </label>
-            <input
-              type="text"
-              id="word"
-              class="block p-2 w-80 text-gray-900 bg-gray-50 rounded-lg 
-                border border-gray-300 sm:text-xs focus:ring-blue-500 focus:border-blue-500 dark:bg-gray-500 
-                dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 
-                dark:focus:border-blue-500"
-              onChange={(e) => setWord(e.target.value)}
-              defaultValue={word}
-            />
+      <div class="p-3">
+        <p class="text-4xl font-bold text-gray-900 dark:text-black">
+          Edit Word
+        </p>
+        <div class="flex flex-row space-x-4">
+          <div class="flex flex-col">
+            <div>
+              <label
+                for="word"
+                class="block mb-2 text-sm font-medium text-black dark:text-black"
+              >
+                Word
+              </label>
+              <input
+                type="text"
+                id="word"
+                class="block p-2 w-80 text-gray-900 bg-gray-50 rounded-lg 
+                    border border-gray-300 sm:text-xs focus:ring-blue-500 focus:border-blue-500 dark:bg-gray-500 
+                    dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 
+                    dark:focus:border-blue-500"
+                onChange={(e) => setWord(e.target.value)}
+                defaultValue={word}
+              />
+            </div>
+            <div>
+              <label
+                for="correctAnswer"
+                class="block mb-2 text-sm font-medium text-black dark:text-black"
+              >
+                Correct Answer
+              </label>
+              <input
+                type="text"
+                id="correctAnswer"
+                class="block p-2 w-80 text-gray-900 bg-gray-50 rounded-lg 
+                    border border-gray-300 sm:text-xs focus:ring-blue-500 focus:border-blue-500 dark:bg-gray-500 
+                    dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 
+                    dark:focus:border-blue-500"
+                onChange={(e) => setCorrectAnswer(e.target.value)}
+                defaultValue={correctAnswer}
+              />
+            </div>
           </div>
-          <div>
-            <label
-              for="correctAnswer"
-              class="block mb-2 text-sm font-medium text-black dark:text-black"
-            >
-              Correct Answer
-            </label>
-            <input
-              type="text"
-              id="correctAnswer"
-              class="block p-2 w-80 text-gray-900 bg-gray-50 rounded-lg 
+          <div class="flex flex-col">
+            <div>
+              <label
+                for="choiceA"
+                class="block mb-2 text-sm font-medium text-black dark:text-black"
+              >
+                Choice A
+              </label>
+              <input
+                type="text"
+                id="choiceA"
+                class="block p-2 w-80 text-gray-900 bg-gray-50 rounded-lg 
+                    border border-gray-300 sm:text-xs focus:ring-blue-500 focus:border-blue-500 dark:bg-gray-500 
+                    dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 
+                    dark:focus:border-blue-500"
+                onChange={(e) => setChoiceA(e.target.value)}
+                defaultValue={choiceA}
+              />
+            </div>
+
+            <div>
+              <label
+                for="choiceB"
+                class="block mb-2 text-sm font-medium text-black dark:text-black"
+              >
+                Choice B
+              </label>
+              <input
+                type="text"
+                id="choiceB"
+                class="block p-2 w-80 text-gray-900 bg-gray-50 rounded-lg 
+                    border border-gray-300 sm:text-xs focus:ring-blue-500 focus:border-blue-500 dark:bg-gray-500 
+                    dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 
+                    dark:focus:border-blue-500"
+                onChange={(e) => setChoiceB(e.target.value)}
+                defaultValue={choiceB}
+              />
+            </div>
+            <div>
+              <label
+                for="choiceC"
+                class="block mb-2 text-sm font-medium text-black dark:text-black"
+              >
+                Choice C
+              </label>
+              <input
+                type="text"
+                id="choiceC"
+                class="block p-2 w-80 text-gray-900 bg-gray-50 rounded-lg 
                 border border-gray-300 sm:text-xs focus:ring-blue-500 focus:border-blue-500 dark:bg-gray-500 
                 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 
                 dark:focus:border-blue-500"
-              onChange={(e) => setCorrectAnswer(e.target.value)}
-              defaultValue={correctAnswer}
-            />
+                onChange={(e) => setChoiceC(e.target.value)}
+                defaultValue={choiceC}
+              />
+            </div>
+            <div>
+              <label
+                for="choiceD"
+                class="block mb-2 text-sm font-medium text-black dark:text-black"
+              >
+                Choice D
+              </label>
+              <input
+                type="text"
+                id="choiceD"
+                class="block p-2 w-80 text-gray-900 bg-gray-50 rounded-lg 
+                    border border-gray-300 sm:text-xs focus:ring-blue-500 focus:border-blue-500 dark:bg-gray-500 
+                    dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 
+                    dark:focus:border-blue-500"
+                onChange={(e) => setChoiceD(e.target.value)}
+                defaultValue={choiceD}
+              />
+            </div>
           </div>
         </div>
 
-        <div class="flex flex-col">
-          <div>
-            <label
-              for="choiceA"
-              class="block mb-2 text-sm font-medium text-black dark:text-black"
-            >
-              Choice A
-            </label>
-            <input
-              type="text"
-              id="choiceA"
-              class="block p-2 w-80 text-gray-900 bg-gray-50 rounded-lg 
-                border border-gray-300 sm:text-xs focus:ring-blue-500 focus:border-blue-500 dark:bg-gray-500 
-                dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 
-                dark:focus:border-blue-500"
-              onChange={(e) => setChoiceA(e.target.value)}
-              defaultValue={choiceA}
-            />
-          </div>
-
-          <div>
-            <label
-              for="choiceB"
-              class="block mb-2 text-sm font-medium text-black dark:text-black"
-            >
-              Choice B
-            </label>
-            <input
-              type="text"
-              id="choiceB"
-              class="block p-2 w-80 text-gray-900 bg-gray-50 rounded-lg 
-                border border-gray-300 sm:text-xs focus:ring-blue-500 focus:border-blue-500 dark:bg-gray-500 
-                dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 
-                dark:focus:border-blue-500"
-              onChange={(e) => setChoiceB(e.target.value)}
-              defaultValue={choiceB}
-            />
-          </div>
-          <div>
-            <label
-              for="choiceC"
-              class="block mb-2 text-sm font-medium text-black dark:text-black"
-            >
-              Choice C
-            </label>
-            <input
-              type="text"
-              id="choiceC"
-              class="block p-2 w-80 text-gray-900 bg-gray-50 rounded-lg 
-                border border-gray-300 sm:text-xs focus:ring-blue-500 focus:border-blue-500 dark:bg-gray-500 
-                dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 
-                dark:focus:border-blue-500"
-              onChange={(e) => setChoiceC(e.target.value)}
-              defaultValue={choiceC}
-            />
-          </div>
-          <div>
-            <label
-              for="choiceD"
-              class="block mb-2 text-sm font-medium text-black dark:text-black"
-            >
-              Choice D
-            </label>
-            <input
-              type="text"
-              id="choiceD"
-              class="block p-2 w-80 text-gray-900 bg-gray-50 rounded-lg 
-                border border-gray-300 sm:text-xs focus:ring-blue-500 focus:border-blue-500 dark:bg-gray-500 
-                dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 
-                dark:focus:border-blue-500"
-              onChange={(e) => setChoiceD(e.target.value)}
-              defaultValue={choiceD}
-            />
-          </div>
-        </div>
+        <button
+          type="button"
+          class="mt-3 text-white bg-blue-700 hover:bg-blue-800 focus:outline-none focus:ring-4 
+            focus:ring-blue-300 font-medium rounded-full text-sm px-8 py-2.5 text-center mr-2 mb-2 
+            dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800"
+          onClick={handleEditWord}
+        >
+          Edit
+        </button>
       </div>
-
-      <button
-        type="button"
-        class="mt-3 text-white bg-blue-700 hover:bg-blue-800 focus:outline-none focus:ring-4 
-        focus:ring-blue-300 font-medium rounded-full text-sm px-5 py-2.5 text-center mr-2 mb-2 
-        dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800"
-        onClick={handleEditWord}
-      >
-        Edit
-      </button>
     </div>
   );
 }
