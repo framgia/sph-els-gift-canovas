cb13a2f773e8e0794a5b3feee6d2d8e1f63a79d0
presyos <preciousgift.canovas@sun-asterisk.com>
2022-11-16 15:42:20 +0800

    [SELS-121] [FE] Not Admin User List

--- frontend/e-learning-fe/src/components/notAdminUsers.js
+++ frontend/e-learning-fe/src/components/notAdminUsers.js
@@ -35,33 +35,33 @@ function NotAdminUsers() {
   }, []);
 
   return (
-    <div class="bg-gray-300 md:h-screen lg:h-screen cursor-default">
+    <div className="bg-gray-300 md:h-screen lg:h-screen cursor-default">
       <Navbar />
-      <h5 class="p-3 text-2xl font-bold tracking-tight text-gray-900 dark:text-black">
+      <h5 className="p-3 text-2xl font-bold tracking-tight text-gray-900 dark:text-black">
         Users
       </h5>
       {isLoading ? (
-        <div class="text-center mt-44">
+        <div className="text-center mt-44">
           <Loader />
         </div>
       ) : (
         <>
           {users.length === 0 ? (
-            <p class="text-4xl text-center mt-44 font-semibold">No Users</p>
+            <p className="text-4xl text-center mt-44 font-semibold">No Users</p>
           ) : (
-            <table class="w-full text-sm text-left text-gray-400 lg:w-3/4 lg:mx-auto md:mx-auto">
-              <thead class="text-xs text-gray-900 uppercase bg-gray-50 dark:bg-gray-700 dark:text-gray-300">
+            <table className="w-full text-sm text-left text-gray-400 lg:w-3/4 lg:mx-auto md:mx-auto">
+              <thead className="text-xs text-gray-900 uppercase bg-gray-50 dark:bg-gray-700 dark:text-gray-300">
                 <tr>
-                  <th scope="col" class="py-3 px-6">
+                  <th scope="col" className="py-3 px-6">
                     Username
                   </th>
-                  <th scope="col" class="py-3 px-6">
+                  <th scope="col" className="py-3 px-6">
                     First Name
                   </th>
-                  <th scope="col" class="py-3 px-6">
+                  <th scope="col" className="py-3 px-6">
                     Last Name
                   </th>
-                  <th scope="col" class="py-3 px-6">
+                  <th scope="col" className="py-3 px-6">
                     Email
                   </th>
                 </tr>
@@ -69,18 +69,19 @@ function NotAdminUsers() {
               <tbody>
                 {users.map((data) => (
                   <tr
-                    class="bg-white border-b dark:bg-gray-800 dark:border-gray-700 cursor-pointer"
+                    key={data.id}
+                    className="bg-white border-b dark:bg-gray-800 dark:border-gray-700 cursor-pointer"
                     onClick={() => handleClickUser(data.username)}
                   >
                     <th
                       scope="row"
-                      class="py-4 px-6 font-medium text-gray-900 whitespace-nowrap dark:text-white"
+                      className="py-4 px-6 font-medium text-gray-900 whitespace-nowrap dark:text-white"
                     >
                       {data.username}
                     </th>
-                    <td class="py-4 px-6">{data.firstname}</td>
-                    <td class="py-4 px-6">{data.lastname}</td>
-                    <td class="py-4 px-6">{data.email}</td>
+                    <td className="py-4 px-6">{data.firstname}</td>
+                    <td className="py-4 px-6">{data.lastname}</td>
+                    <td className="py-4 px-6">{data.email}</td>
                   </tr>
                 ))}
               </tbody>
