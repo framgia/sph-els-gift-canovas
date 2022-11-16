f9b9281b1664d042fc3147aba4e5d0ae6eed2962
presyos <preciousgift.canovas@sun-asterisk.com>
2022-11-16 15:12:27 +0800

    [SELS-116] [FE] ADMIN - Words Per Category

--- frontend/e-learning-fe/src/components/admin/wordsPerCategory.js
+++ frontend/e-learning-fe/src/components/admin/wordsPerCategory.js
@@ -53,7 +53,7 @@ function AdminWordsPerCategory() {
   }, []);
 
   return (
-    <div class="bg-gray-300 md:h-screen lg:h-screen cursor-default">
+    <div className="bg-gray-300 md:h-screen lg:h-screen cursor-default">
       <ToastContainer
         position="top-right"
         autoClose={5000}
@@ -66,73 +66,73 @@ function AdminWordsPerCategory() {
         pauseOnHover
       />
       <Navbar />
-      <div class="p-5">
-        <h5 class="mb-2 text-2xl font-bold tracking-tight text-gray-900 dark:text-black">
+      <div className="p-5">
+        <h5 className="mb-2 text-2xl font-bold tracking-tight text-gray-900 dark:text-black">
           Words in Category : {words.category_name}
         </h5>
         {isLoading ? (
-          <div class="text-center mt-44">
+          <div className="text-center mt-44">
             <Loader />
           </div>
         ) : (
-          <div class="flex flex-col place-items-center mt-5">
-            <ul class="w-full text-xl font-medium text-gray-900 bg-white rounded-lg border border-gray-200 dark:bg-gray-700 dark:border-gray-600 dark:text-white">
-              <div class="flex flex-row">
-                <li class="py-2 px-4 w-6/12 rounded-t-lg border-b border-gray-200 dark:border-gray-600">
+          <div className="flex flex-col place-items-center mt-5">
+            <ul className="w-full text-xl font-medium text-gray-900 bg-white rounded-lg border border-gray-200 dark:bg-gray-700 dark:border-gray-600 dark:text-white">
+              <div className="flex flex-row">
+                <li className="py-2 px-4 w-6/12 rounded-t-lg border-b border-gray-200 dark:border-gray-600">
                   Word
                 </li>
-                <li class="py-2 px-4 w-3/4 border-b border-gray-200 dark:border-gray-600">
+                <li className="py-2 px-4 w-3/4 border-b border-gray-200 dark:border-gray-600">
                   Correct Answer
                 </li>
-                <li class="py-2 px-4 w-3/4 border-b border-gray-200 dark:border-gray-600">
+                <li className="py-2 px-4 w-3/4 border-b border-gray-200 dark:border-gray-600">
                   Choice A
                 </li>
-                <li class="py-2 px-4 w-3/4 border-b border-gray-200 dark:border-gray-600">
+                <li className="py-2 px-4 w-3/4 border-b border-gray-200 dark:border-gray-600">
                   Choice B
                 </li>
-                <li class="py-2 px-4 w-3/4 border-b border-gray-200 dark:border-gray-600">
+                <li className="py-2 px-4 w-3/4 border-b border-gray-200 dark:border-gray-600">
                   Choice C
                 </li>
-                <li class="py-2 px-4 w-3/4 border-b border-gray-200 dark:border-gray-600">
+                <li className="py-2 px-4 w-3/4 border-b border-gray-200 dark:border-gray-600">
                   Choice D
                 </li>
-                <li class="py-2 px-4 w-3/4 border-b border-gray-200 dark:border-gray-600">
+                <li className="py-2 px-4 w-3/4 border-b border-gray-200 dark:border-gray-600">
                   Action
                 </li>
               </div>
             </ul>
-            <ul class="w-full text-sm font-medium text-gray-900 bg-white rounded-lg border border-gray-200 dark:bg-gray-800 dark:border-gray-600 dark:text-white mt-8">
+            <ul className="w-full text-sm font-medium text-gray-900 bg-white rounded-lg border border-gray-200 dark:bg-gray-800 dark:border-gray-600 dark:text-white mt-8">
               {words.words.map((word) => (
-                <div class="flex flex-row">
-                  <li class="py-2 px-4 w-6/12 rounded-t-lg border-b border-gray-200 dark:border-gray-600">
+                <div key={word.word_id} className="flex flex-row">
+                  <li className="py-2 px-4 w-6/12 rounded-t-lg border-b border-gray-200 dark:border-gray-600">
                     {word.word}
                   </li>
-                  <li class="py-2 px-4 w-3/4 border-b border-gray-200 dark:border-gray-600">
+                  <li className="py-2 px-4 w-3/4 border-b border-gray-200 dark:border-gray-600">
                     {word.correct_answer}
                   </li>
-                  <li class="py-2 px-4 w-3/4 border-b border-gray-200 dark:border-gray-600">
+                  <li className="py-2 px-4 w-3/4 border-b border-gray-200 dark:border-gray-600">
                     {word.choice_a}
                   </li>
-                  <li class="py-2 px-4 w-3/4 border-b border-gray-200 dark:border-gray-600">
+                  <li className="py-2 px-4 w-3/4 border-b border-gray-200 dark:border-gray-600">
                     {word.choice_b}
                   </li>
-                  <li class="py-2 px-4 w-3/4 border-b border-gray-200 dark:border-gray-600">
+                  <li className="py-2 px-4 w-3/4 border-b border-gray-200 dark:border-gray-600">
                     {word.choice_c}
                   </li>
-                  <li class="py-2 px-4 w-3/4 border-b border-gray-200 dark:border-gray-600">
+                  <li className="py-2 px-4 w-3/4 border-b border-gray-200 dark:border-gray-600">
                     {word.choice_d}
                   </li>
-                  <li class="py-2 px-4 w-3/4 border-b border-gray-200 dark:border-gray-600">
-                    <div class="flex flex-row space-x-3 cursor-pointer">
+                  <li className="py-2 px-4 w-3/4 border-b border-gray-200 dark:border-gray-600">
+                    <div className="flex flex-row space-x-3 cursor-pointer">
                       <Link
                         to="/admin-edit-word-and-choices"
                         state={{ data: word }}
                       >
-                        <p class="underline">Edit Word</p>
+                        <p className="underline">Edit Word</p>
                       </Link>
-                      <p class="underline">|</p>
+                      <p className="underline">|</p>
                       <p
-                        class="underline"
+                        className="underline"
                         onClick={() => handleDeleteWord(word.word_id)}
                       >
                         Delete Word
