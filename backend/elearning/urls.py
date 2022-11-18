9575329426cd753afdcf5b7a1cdd72e76935574b
presyos <preciousgift.canovas@sun-asterisk.com>
2022-11-18 11:51:48 +0800

    [SELS-133] [BE] Unit testing Register

--- backend/elearning/urls.py
+++ backend/elearning/urls.py
@@ -37,7 +37,7 @@ urlpatterns = [
     path("follow/", AddNewFollower.as_view()),
     path("get_words/<int:category_id>", GetWordsPerCategory.as_view()),
     path("not_admin_users/<str:username>", NotAdminUserList.as_view()),
-    path("register/", RegisterView.as_view()),
+    path("register/", RegisterView.as_view(), name="register"),
     path("results/<int:quiz_taken_id>", GetResults.as_view()),
     path("user_answer/", UserAnswerView.as_view()),
     path("edit_user_details/<int:id>/", EditUserDetails.as_view()),
