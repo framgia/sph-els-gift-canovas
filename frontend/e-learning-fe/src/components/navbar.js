6aeddcacd10e5f1c921f689a9d7a8d9401fe1155
presyos <112840596+presyos@users.noreply.github.com>
2022-11-21 10:21:40 +0800

    [SELS-120] [FE] Navbar (#125)

--- frontend/e-learning-fe/src/components/navbar.js
+++ frontend/e-learning-fe/src/components/navbar.js
@@ -7,36 +7,36 @@ function Navbar() {
     navigate("/", { replace: true });
   };
   return (
-    <nav class="relative w-full p-6 dark:bg-gray-900 border-border-gray-200 dark:border-gray-600">
-      <div class="flex items-center justify-between flex flex-wrap">
+    <nav className="relative w-full p-6 dark:bg-gray-900 border-border-gray-200 dark:border-gray-600">
+      <div className="flex items-center justify-between flex flex-wrap">
         <Link to="/dashboard">
-          <span class="self-center text-xl font-semibold dark:text-white">
+          <span className="self-center text-xl font-semibold dark:text-white">
             E-Learning Admin
           </span>
         </Link>
-        <div class="sm:flex md:flex space-x-6" id="navbar-sticky">
+        <div className="sm:flex md:flex space-x-6" id="navbar-sticky">
           <Link to="/dashboard">
-            <p class="block py-2 pr-4 pl-3 text-white md:text-white text-base md:p-0 dark:text-white">
+            <p className="block py-2 pr-4 pl-3 text-white md:text-white text-base md:p-0 dark:text-white">
               Home
             </p>
           </Link>
           <Link to="/categories">
-            <p class="block py-2 pr-4 pl-3 text-white md:text-white text-base md:p-0 dark:text-white">
+            <p className="block py-2 pr-4 pl-3 text-white md:text-white text-base md:p-0 dark:text-white">
               Categories
             </p>
           </Link>
           <Link to="/users">
-            <p class="block py-2 pr-4 pl-3 text-white md:text-white text-base md:p-0 dark:text-white">
+            <p className="block py-2 pr-4 pl-3 text-white md:text-white text-base md:p-0 dark:text-white">
               Users
             </p>
           </Link>
           <Link to="/profile">
-            <p class="block py-2 pr-4 pl-3 text-white md:text-white text-base md:p-0 dark:text-white">
+            <p className="block py-2 pr-4 pl-3 text-white md:text-white text-base md:p-0 dark:text-white">
               Profile
             </p>
           </Link>
           <p
-            class="block py-2 pr-4 pl-3 text-white md:text-white text-base md:p-0 dark:text-white cursor-pointer"
+            className="block py-2 pr-4 pl-3 text-white md:text-white text-base md:p-0 dark:text-white cursor-pointer"
             onClick={handleLogout}
           >
             Logout
