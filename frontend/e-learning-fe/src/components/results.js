8c570e36ba5c1155abc41f4c066bde22f4ecebe8
presyos <112840596+presyos@users.noreply.github.com>
2022-11-21 10:23:38 +0800

    [SELS-123] [FE] Results (#128)

--- frontend/e-learning-fe/src/components/results.js
+++ frontend/e-learning-fe/src/components/results.js
@@ -30,58 +30,58 @@ function Results() {
   }, []);
 
   return (
-    <div class="bg-gray-300 md:h-screen lg:h-screen cursor-default">
+    <div className="bg-gray-300 md:h-screen lg:h-screen cursor-default">
       <Navbar />
-      <h5 class="p-3 text-2xl font-bold tracking-tight text-gray-900 dark:text-black">
+      <h5 className="p-3 text-2xl font-bold tracking-tight text-gray-900 dark:text-black">
         Results
       </h5>
       {isLoading ? (
-        <div class="text-center mt-44">
+        <div className="text-center mt-44">
           <Loader />
         </div>
       ) : (
-        <div class="flex flex-col place-items-center">
+        <div className="flex flex-col place-items-center">
           <ul
-            class="w-8/12 text-xl font-medium text-gray-900 bg-white rounded-lg border
+            className="w-8/12 text-xl font-medium text-gray-900 bg-white rounded-lg border
             border-gray-200 dark:bg-gray-700 dark:border-gray-600 dark:text-white"
           >
-            <div class="flex flex-row">
-              <li class="py-2 px-4 w-full rounded-t-lg border-b border-gray-200 dark:border-gray-600"></li>
-              <li class="py-2 px-4 w-full rounded-t-lg border-b border-gray-200 dark:border-gray-600">
+            <div className="flex flex-row">
+              <li className="py-2 px-4 w-full rounded-t-lg border-b border-gray-200 dark:border-gray-600"></li>
+              <li className="py-2 px-4 w-full rounded-t-lg border-b border-gray-200 dark:border-gray-600">
                 Word
               </li>
-              <li class="py-2 px-4 w-full border-b border-gray-200 dark:border-gray-600">
+              <li className="py-2 px-4 w-full border-b border-gray-200 dark:border-gray-600">
                 Answer
               </li>
             </div>
           </ul>
           <ul
-            class="w-8/12 text-sm font-medium text-gray-900 bg-white rounded-lg border
+            className="w-8/12 text-sm font-medium text-gray-900 bg-white rounded-lg border
               border-gray-200 dark:bg-gray-600 dark:border-gray-600 dark:text-white mt-8"
           >
             {results.map((data) => (
-              <div class="flex flex-row">
+              <div key={data.id} className="flex flex-row">
                 {data.is_correct === true ? (
-                  <li class="py-2 px-4 w-full rounded-t-lg border-b border-gray-200 dark:border-gray-600">
+                  <li className="py-2 px-4 w-full rounded-t-lg border-b border-gray-200 dark:border-gray-600">
                     <svg
-                      class="w-6 h-6"
+                      className="w-6 h-6"
                       fill="none"
                       stroke="currentColor"
                       viewBox="0 0 24 24"
                       xmlns="http://www.w3.org/2000/svg"
                     >
                       <path
-                        stroke-linecap="round"
-                        stroke-linejoin="round"
-                        stroke-width="2"
+                        strokeLinecap="round"
+                        strokeLinejoin="round"
+                        strokeWidth="2"
                         d="M9 12l2 2 4-4m6 2a9 9 0 11-18 0 9 9 0 0118 0z"
                       ></path>
                     </svg>
                   </li>
                 ) : (
-                  <li class="py-2 px-4 w-full rounded-t-lg border-b border-gray-200 dark:border-gray-600">
+                  <li className="py-2 px-4 w-full rounded-t-lg border-b border-gray-200 dark:border-gray-600">
                     <svg
-                      class="w-6 h-6"
+                      className="w-6 h-6"
                       fill="none"
                       stroke="currentColor"
                       viewBox="0 0 24 24"
@@ -96,10 +96,10 @@ function Results() {
                     </svg>
                   </li>
                 )}
-                <li class="py-2 px-4 w-full rounded-t-lg border-b border-gray-200 dark:border-gray-600">
+                <li className="py-2 px-4 w-full rounded-t-lg border-b border-gray-200 dark:border-gray-600">
                   {data.word}
                 </li>
-                <li class="py-2 px-4 w-full rounded-t-lg border-b border-gray-200 dark:border-gray-600">
+                <li className="py-2 px-4 w-full rounded-t-lg border-b border-gray-200 dark:border-gray-600">
                   {data.user_answer}
                 </li>
               </div>
