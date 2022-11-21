4d18698486c98d61bbe4cecbf6579552c424f7d2
presyos <112840596+presyos@users.noreply.github.com>
2022-11-21 10:17:44 +0800

    [SELS-115] [FE] ADMIN - Navbar (#120)

--- frontend/e-learning-fe/src/components/admin/navbar.js
+++ frontend/e-learning-fe/src/components/admin/navbar.js
@@ -7,26 +7,26 @@ function AdminNavbar() {
     navigate("/", { replace: true });
   };
   return (
-    <nav class="relative w-full p-6 dark:bg-gray-900 border-border-gray-200 dark:border-gray-600">
-      <div class="flex items-center justify-between flex flex-wrap">
+    <nav className="relative w-full p-6 dark:bg-gray-900 border-border-gray-200 dark:border-gray-600">
+      <div className="flex items-center justify-between flex flex-wrap">
         <Link to="/admin-dashboard">
-          <span class="self-center text-xl font-semibold dark:text-white ">
+          <span className="self-center text-xl font-semibold dark:text-white ">
             E-Learning Admin
           </span>
         </Link>
-        <div class="sm:flex md:flex space-x-6 cursor-pointer">
+        <div className="sm:flex md:flex space-x-6 cursor-pointer">
           <Link to="/admin-dashboard">
-            <p class="block py-2 pr-4 pl-3 text-white md:text-white text-base md:p-0 dark:text-white">
+            <p className="block py-2 pr-4 pl-3 text-white md:text-white text-base md:p-0 dark:text-white">
               Home
             </p>
           </Link>
           <Link to="/admin-users">
-            <p class="block py-2 pr-4 pl-3 text-white md:text-white text-base md:p-0 dark:text-white">
+            <p className="block py-2 pr-4 pl-3 text-white md:text-white text-base md:p-0 dark:text-white">
               Users
             </p>
           </Link>
           <p
-            class="block py-2 pr-4 pl-3 text-white md:text-white text-base md:p-0 dark:text-white"
+            className="block py-2 pr-4 pl-3 text-white md:text-white text-base md:p-0 dark:text-white"
             onClick={handleLogout}
           >
             Logout
