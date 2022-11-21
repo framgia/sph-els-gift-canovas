fbded274e44451bc97d58c17ed191eefea9f79a6
presyos <112840596+presyos@users.noreply.github.com>
2022-11-21 10:16:03 +0800

    [SELS-112] [FE] ADMIN - Dashboard (#117)

--- frontend/e-learning-fe/src/components/admin/dashboard.js
+++ frontend/e-learning-fe/src/components/admin/dashboard.js
@@ -49,7 +49,7 @@ function AdminDashboard() {
   }, []);
 
   return (
-    <div class="bg-gray-300 md:h-screen lg:h-screen cursor-default">
+    <div className="bg-gray-300 md:h-screen lg:h-screen cursor-default">
       <ToastContainer
         position="top-right"
         autoClose={5000}
@@ -62,72 +62,75 @@ function AdminDashboard() {
         pauseOnHover
       />
       <Navbar />
-      <h5 class="text-2xl font-bold tracking-tight text-gray-900 dark:text-black p-2">
+      <h5 className="text-2xl font-bold tracking-tight text-gray-900 dark:text-black p-2">
         Categories
       </h5>
 
       {isLoading ? (
-        <div class="text-center mt-44">
+        <div className="text-center mt-44">
           <Loader />
         </div>
       ) : (
-        <div class="relative lg:mx-auto">
-          <div class="w-full flex justify-end">
+        <div className="relative lg:mx-auto">
+          <div className="w-full flex justify-end">
             <Link to="/admin-add-category">
-              <button class="mt-2 inline-flex items-center justify-center p-0.5 mb-2 mr-2 overflow-hidden text-sm font-medium text-gray-900 rounded-lg group bg-gradient-to-br from-cyan-500 to-blue-500 group-hover:from-cyan-500 group-hover:to-blue-500 hover:text-white dark:text-white focus:ring-4 focus:outline-none focus:ring-cyan-200 dark:focus:ring-cyan-800">
-                <span class="px-5 py-2.5 transition-all ease-in duration-75 bg-white dark:bg-gray-900 rounded-md group-hover:bg-opacity-0">
+              <button className="mt-2 inline-flex items-center justify-center p-0.5 mb-2 mr-2 overflow-hidden text-sm font-medium text-gray-900 rounded-lg group bg-gradient-to-br from-cyan-500 to-blue-500 group-hover:from-cyan-500 group-hover:to-blue-500 hover:text-white dark:text-white focus:ring-4 focus:outline-none focus:ring-cyan-200 dark:focus:ring-cyan-800">
+                <span className="px-5 py-2.5 transition-all ease-in duration-75 bg-white dark:bg-gray-900 rounded-md group-hover:bg-opacity-0">
                   Add Category
                 </span>
               </button>
             </Link>
           </div>
-          <table class="w-full text-sm text-left text-gray-400 lg:w-3/4 lg:mx-auto md:mx-auto">
-            <thead class="text-xs text-gray-900 uppercase bg-gray-50 dark:bg-gray-700 dark:text-gray-300">
+          <table className="w-full text-sm text-left text-gray-400 lg:w-3/4 lg:mx-auto md:mx-auto">
+            <thead className="text-xs text-gray-900 uppercase bg-gray-50 dark:bg-gray-700 dark:text-gray-300">
               <tr>
-                <th scope="col" class="py-3 px-6">
+                <th scope="col" className="py-3 px-6">
                   Title
                 </th>
-                <th scope="col" class="py-3 px-6">
+                <th scope="col" className="py-3 px-6">
                   Description
                 </th>
-                <th scope="col" class="py-3 px-6">
+                <th scope="col" className="py-3 px-6">
                   Action
                 </th>
               </tr>
             </thead>
             <tbody>
               {categories.map((category) => (
-                <tr class="bg-white border-b dark:bg-gray-800 dark:border-gray-700">
-                  <Link
-                    to="/admin-words-per-category"
-                    state={{ data: category }}
+                <tr
+                  key={category.id}
+                  className="bg-white border-b dark:bg-gray-800 dark:border-gray-700"
+                >
+                  <th
+                    scope="row"
+                    className="py-4 px-6 font-medium text-gray-900 whitespace-nowrap dark:text-white"
                   >
-                    <th
-                      scope="row"
-                      class="py-4 px-6 font-medium text-gray-900 whitespace-nowrap dark:text-white"
+                    <Link
+                      to="/admin-words-per-category"
+                      state={{ data: category }}
                     >
                       {category.category_name}
-                    </th>
-                  </Link>
-                  <td class="py-4 px-6"> {category.description}</td>
-                  <td class="py-4 px-6">
-                    <div class="flex flex-row space-x-3">
+                    </Link>
+                  </th>
+                  <td className="py-4 px-6"> {category.description}</td>
+                  <td className="py-4 px-6">
+                    <div className="flex flex-row space-x-3">
                       <Link
                         to="/admin-add-word-choices"
                         state={{ data: category }}
                       >
-                        <p class="underline">Add Word</p>
+                        <p className="underline">Add Word</p>
                       </Link>
-                      <p class="underline">|</p>
+                      <p className="underline">|</p>
                       <Link
                         to="/admin-edit-category"
                         state={{ data: category }}
                       >
-                        <p class="underline">Edit Category</p>
+                        <p className="underline">Edit Category</p>
                       </Link>
-                      <p class="underline">|</p>
+                      <p className="underline">|</p>
                       <p
-                        class="underline cursor-pointer"
+                        className="underline cursor-pointer"
                         onClick={() => handleDeleteCategory(category.id)}
                       >
                         Delete Category
