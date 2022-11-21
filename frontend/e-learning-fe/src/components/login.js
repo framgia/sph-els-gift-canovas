93b01f26ebe5c40502b5c7f09ce1957beee8fb68
presyos <112840596+presyos@users.noreply.github.com>
2022-11-21 10:42:28 +0800

    [SELS-127] [FE] Login (#132)

--- frontend/e-learning-fe/src/components/login.js
+++ frontend/e-learning-fe/src/components/login.js
@@ -60,7 +60,7 @@ function Login() {
   }, []);
 
   return (
-    <section class="bg-gray-50 dark:bg-gray-900">
+    <section className="bg-gray-50 dark:bg-gray-900">
       <ToastContainer
         position="top-right"
         autoClose={5000}
@@ -72,21 +72,21 @@ function Login() {
         draggable
         pauseOnHover
       />
-      <div class="flex flex-col items-center justify-center px-6 py-8 mx-auto md:h-screen lg:py-0">
+      <div className="flex flex-col items-center justify-center px-6 py-8 mx-auto md:h-screen lg:py-0">
         {isLoading ? <Loader /> : ""}
-        <p class="flex items-center mb-6 text-2xl font-semibold text-gray-900 dark:text-white mt-5">
+        <p className="flex items-center mb-6 text-2xl font-semibold text-gray-900 dark:text-white mt-5">
           E-Learning
         </p>
-        <div class="w-96 bg-white rounded-lg shadow dark:border md:mt-0 sm:max-w-md xl:p-0 dark:bg-gray-800 dark:border-gray-700">
-          <div class="p-6 space-y-4 md:space-y-6 sm:p-8">
-            <h1 class="text-xl font-bold leading-tight tracking-tight text-gray-900 md:text-2xl dark:text-white">
+        <div className="w-96 bg-white rounded-lg shadow dark:border md:mt-0 sm:max-w-md xl:p-0 dark:bg-gray-800 dark:border-gray-700">
+          <div className="p-6 space-y-4 md:space-y-6 sm:p-8">
+            <h1 className="text-xl font-bold leading-tight tracking-tight text-gray-900 md:text-2xl dark:text-white">
               Sign in to your account
             </h1>
 
             <div>
               <label
-                for="username"
-                class="block mb-2 text-sm font-medium text-gray-900 dark:text-white"
+                htmlFor="username"
+                className="block mb-2 text-sm font-medium text-gray-900 dark:text-white"
               >
                 Username
               </label>
@@ -94,7 +94,7 @@ function Login() {
                 type="username"
                 name="username"
                 id="username"
-                class="bg-gray-50 border border-gray-300 text-gray-900 sm:text-sm rounded-lg 
+                className="bg-gray-50 border border-gray-300 text-gray-900 sm:text-sm rounded-lg 
                       focus:ring-primary-600 focus:border-primary-600 block w-full p-2.5 dark:bg-gray-700 
                       dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 
                       dark:focus:border-blue-500"
@@ -108,8 +108,8 @@ function Login() {
             </div>
             <div>
               <label
-                for="password"
-                class="block mb-2 text-sm font-medium text-gray-900 dark:text-white"
+                htmlFor="password"
+                className="block mb-2 text-sm font-medium text-gray-900 dark:text-white"
               >
                 Password
               </label>
@@ -118,7 +118,7 @@ function Login() {
                 name="password"
                 id="password"
                 placeholder="••••••••"
-                class="bg-gray-50 border border-gray-300 text-gray-900 sm:text-sm rounded-lg 
+                className="bg-gray-50 border border-gray-300 text-gray-900 sm:text-sm rounded-lg 
                       focus:ring-primary-600 focus:border-primary-600 block w-full p-2.5 dark:bg-gray-700 
                       dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 
                       dark:focus:border-blue-500"
@@ -131,7 +131,7 @@ function Login() {
             </div>
             <button
               type="submit"
-              class={`w-full text-white bg-primary-600 hover:bg-primary-700 focus:ring-4 focus:outline-none focus:ring-primary-300 font-medium rounded-lg text-sm px-5 py-2.5 text-center dark:hover:bg-primary-700 dark:focus:ring-primary-800 ${
+              className={`w-full text-white bg-primary-600 hover:bg-primary-700 focus:ring-4 focus:outline-none focus:ring-primary-300 font-medium rounded-lg text-sm px-5 py-2.5 text-center dark:hover:bg-primary-700 dark:focus:ring-primary-800 ${
                 isDisableSignInButton ? "dark:bg-gray-600" : "dark:bg-blue-600"
               }`}
               onClick={handleLogin}
@@ -139,14 +139,14 @@ function Login() {
             >
               Sign in
             </button>
-            <p class="text-sm font-light text-gray-500 dark:text-gray-400">
+            <h1 className="font-light text-gray-500 dark:text-gray-400">
               Don’t have an account yet?
               <Link to="/signup">
-                <p class="font-medium text-primary-600 hover:underline dark:text-primary-500">
+                <p className="font-medium text-primary-600 hover:underline dark:text-primary-500">
                   Sign up
                 </p>
               </Link>
-            </p>
+            </h1>
           </div>
         </div>
       </div>
