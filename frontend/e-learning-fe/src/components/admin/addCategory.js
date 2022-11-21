9937c6e946e36d557db59fd4cd6ba2b238ffbe80
presyos <112840596+presyos@users.noreply.github.com>
2022-11-21 10:13:03 +0800

    [SELS-109] [FE] ADMIN - Add Category (#114)

--- frontend/e-learning-fe/src/components/admin/addCategory.js
+++ frontend/e-learning-fe/src/components/admin/addCategory.js
@@ -40,7 +40,7 @@ function AdminAddCategory() {
   };
 
   return (
-    <div class="bg-gray-300 h-screen cursor-default">
+    <div className="bg-gray-300 h-screen cursor-default">
       <ToastContainer
         position="top-right"
         autoClose={5000}
@@ -53,20 +53,20 @@ function AdminAddCategory() {
         pauseOnHover
       />
       <Navbar />
-      <p class="text-4xl p-6 font-bold text-gray-900 dark:text-black">
+      <p className="text-4xl p-6 font-bold text-gray-900 dark:text-black">
         Add Categories
       </p>
-      <div class="p-6">
+      <div className="p-6">
         <label
-          for="categoryName"
-          class="block mb-2 text-sm font-medium text-black dark:text-black"
+          htmlFor="categoryName"
+          className="block mb-2 text-sm font-medium text-black dark:text-black"
         >
           Category Name
         </label>
         <input
           type="text"
           id="categoryName"
-          class="block p-2 w-80 text-gray-900 bg-gray-50 rounded-lg 
+          className="block p-2 w-80 text-gray-900 bg-gray-50 rounded-lg 
                 border border-gray-300 sm:text-xs focus:ring-blue-500 focus:border-blue-500 dark:bg-gray-500 
                 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 
                 dark:focus:border-blue-500"
@@ -76,17 +76,17 @@ function AdminAddCategory() {
           }}
         />
       </div>
-      <div class="p-6">
+      <div className="p-6">
         <label
-          for="description"
-          class="block mb-2 text-sm font-medium text-black dark:text-black"
+          htmlFor="description"
+          className="block mb-2 text-sm font-medium text-black dark:text-black"
         >
           Description
         </label>
         <textarea
           id="message"
           rows="4"
-          class="block p-2 w-80 text-gray-900 bg-gray-50 rounded-lg 
+          className="block p-2 w-80 text-gray-900 bg-gray-50 rounded-lg 
         border border-gray-300 sm:text-xs focus:ring-blue-500 focus:border-blue-500 dark:bg-gray-500 
         dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 
         dark:focus:border-blue-500"
@@ -98,7 +98,7 @@ function AdminAddCategory() {
       </div>
       <button
         type="button"
-        class={`ml-5 text-white focus:outline-none focus:ring-4 focus:ring-blue-300 
+        className={`ml-5 text-white focus:outline-none focus:ring-4 focus:ring-blue-300 
         font-medium rounded-full text-sm px-8 py-2.5 text-center ${
           isDisableAdd ? "bg-gray-700" : "bg-blue-700"
         }`}
